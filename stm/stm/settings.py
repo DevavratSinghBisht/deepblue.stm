11f2a891766b9d737ffb6ac37b74614ad7693e33
DevavratSinghBisht <devavratsinghbisht@gmail.com>
2022-04-09 04:26:43 +0530

    Set Debug Mode False

--- stm/stm/settings.py
+++ stm/stm/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ui$=7hgawerp$=bqo@fh6j=&krv!1n1yu-m#rht8y=lnyaqo*a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
 
 
 # Application definition
